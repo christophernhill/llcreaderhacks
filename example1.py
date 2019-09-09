2e4d25593194b2936f8bfac943398ef92906899f
Chris Hill <cnh@mit.edu>
2019-09-09 05:16:42 -0700

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,5 +1,13 @@
 from xmitgcm import llcreader
 model = llcreader.ECCOPortalLLC4320Model()
 ds1 = model.get_dataset(varnames=['Eta'])
-phi=ds1.Eta[1,1,1:4320,1:4320]
-phi.to_netcdf('foo.nc')
+tsLo=1
+tsHi=1
+fLo=1
+fHi=1
+jLo=1
+jHi=1
+iLo=1
+iHi=4320
+phi=ds1.Eta[tsLo:tsHi,fLo:fHi,jLo:jHi,iLo:iHi]
+phi.to_netcdf('eta.nc')
